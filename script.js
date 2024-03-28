57fcc714f2bd2e9885edc0039608ea9fbd537e41
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-03-28 23:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+let btn = document.getElementById("btn");
+let output = document.getElementById("output");
+
+
+btn.addEventListener("click",fun);
+
+async function fun(){
+		try{
+			let data = await checker();
+			output.innerHTML = data;
+		}catch(e){
+			console.log(e);
+		}
+}
+
+function checker(){
+	return new Promise((resolve)=>{
+		
+setTimeout(()=>{
+	// console.log(text);
+	resolve(text.value);
+},Number(delay.value))
+	})
+}
